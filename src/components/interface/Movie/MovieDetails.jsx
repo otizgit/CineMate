23e5d3d3190aa6b9ac9507693e802ee0b1b929cb
otizgit <emmanueljoe777@gmail.com>
2025-02-13 20:58:29 +0100

    responsiveness fix

--- src/components/interface/Movie/MovieDetails.jsx
+++ src/components/interface/Movie/MovieDetails.jsx
@@ -329,7 +329,7 @@ export default function MovieDetails(props) {
               scale: 1.1,
             }}
             className={`flex items-center gap-1 ${
-              trailerVideo.length ? "" : "mb-5"
+              trailerVideo.length ? "" : "mb-5 lg:mb-0"
             }`}
           >
             <FontAwesomeIcon
